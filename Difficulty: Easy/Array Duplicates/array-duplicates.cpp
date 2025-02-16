f0a03ffb457056d963409e5f5f83fa184481f763
Saumya Chauhan <155440810+ChauhanSaumya@users.noreply.github.com>
2025-02-16 09:53:25 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Array Duplicates/array-duplicates.cpp
@@ -0,0 +1,73 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    vector<int> findDuplicates(vector<int>& arr) {
+        // code here
+        int n=arr.size();
+        int cnt=0;
+        for(int i=0;i<n;i++)
+        {
+              unordered_map<int, int> countMap; // To store the count of each element
+             vector<int> duplicates; // To store the duplicates
+
+            // Count occurrences of each element
+            for (int num : arr) {
+            countMap[num]++;
+        }
+
+            // Find elements that appear more than once
+            for (const auto& pair : countMap) {
+            if (pair.second > 1) {
+            duplicates.push_back(pair.first);
+        }
+        }
+
+    return duplicates; // Return the list of duplicates
+    }
+        }   
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t; // Number of test cases
+    cin >> t;
+    cin.ignore(); // Ignore the newline after the integer input
+    while (t--) {
+        vector<int> a;
+        string input;
+
+        // Input format: first number n followed by the array elements
+        getline(cin, input);
+        stringstream ss(input);
+        int num;
+        while (ss >> num)
+            a.push_back(num); // Read the array elements from input string
+
+        Solution obj;
+        vector<int> result = obj.findDuplicates(a);
+        sort(result.begin(), result.end());
+
+        // Print the result in the required format
+        if (result.empty()) {
+            cout << "[]"; // Print empty brackets if no duplicates are found
+        } else {
+            for (int i = 0; i < result.size(); i++) {
+                if (i != 0)
+                    cout << " ";
+                cout << result[i];
+            }
+        }
+        cout << endl;
+        cout << "~" << endl;
+    }
+
+    return 0;
+}
+
+// } Driver Code Ends
